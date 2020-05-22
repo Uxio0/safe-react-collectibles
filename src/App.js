766b9590e3bfd92b29f2405b723d9165b90cc19a
Uxio Fuentefria <uxio@gnosis.pm>
2020-05-22 18:22:05 +0200

    Implement collectibles basic app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+
+import initSdk from '@gnosis.pm/safe-apps-sdk'
 import './App.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  
+}));
+
+
+function Collectibles() {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [safeInfo, setSafeInfo] = useState();
+  const [appsSdk] = useState(initSdk());
+
+  useEffect(() => {
+    if (safeInfo !== undefined) {
+      const network = safeInfo.network.toLowerCase() == 'mainnet'?'mainnet.':''
+      fetch(`https://safe-transaction.${network}staging.gnosisdev.com/api/v1/safes/${safeInfo.safeAddress}/collectibles/`)
+        .then(response => response.json())
+        .then(data => {setLoading(false); setData(data)});
+    }
+  }, [safeInfo]);
+
+
+  useEffect(() => {
+    appsSdk.addListeners({
+      onSafeInfo: setSafeInfo,
+    });
+
+    return () => appsSdk.removeListeners();
+  }, [appsSdk]);
+
+  
+  return (
+    <div>
+      {loading && <CircularProgress />}
+      <GridList cellHeight={200} className={classes.gridList} cols={3}>
+        {data.map((el) => (
+          <GridListTile key={`${el.address}-${el.id}`} cols={1}>
+            <img style={{height: '150px', 'object-fit': 'contain'}} src={el.imageUri} alt={el.description} />
+            <GridListTileBar
+              title={el.name}
+              subtitle={<span>{el.description}</span>}
+              actionIcon={
+                <a href={el.uri} target="_blank">
+                  <IconButton aria-label={el.uri} className={classes.icon}>
+                    <InfoIcon />
+                  </IconButton>
+                </a>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Collectibles />
     </div>
   );
 }
